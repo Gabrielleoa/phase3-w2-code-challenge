a664e98af2519c543faf4a9b86a23a2ccf66473c
Gabrielle <gabrielle.odhiambo@student.moringaschool.com>
2023-12-18 08:44:59 +0300

    restaurant and review

--- Restaurant.py
+++ Restaurant.py
@@ -1,13 +1,21 @@
 class Restaurant:
+    restuarants=[]
     def __init__(self, name):
         self._name= name
+        Restaurant.restuarants.append(self)
     def restaurant_name(self):
         return self._name
+    @classmethod
+    def all(cls):
+        return cls.restuarants
+    def average_star_rating(self):
+        return
     def customer(self, customer):
         self.customer= customer
         return self.customer
     def reviews(self):
         return self._name
+    
     
 restaurant= Restaurant(name='La Bodega')
 print(restaurant._name)
--- Review.py
+++ Review.py
@@ -1,12 +1,14 @@
 class Review:
+    reviews=[]
     def __init__(self, customer, restuarant, rating):
         self._customer = customer
         self._restaurant = restuarant
         self.rating = rating
+        Review.reviews.append(self)
     def rating(self):
         return self.rating
-    def all_reviews(self):
-        return (f'{self.restaurant}' + f'{self.customer}' + f'{self.rating}')
+    def all(cls):
+        return cls.reviews
     def customer(self):
         return self._customer
     def restaurant(self):
