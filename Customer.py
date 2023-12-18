66bddd8b168566cc3dd8b8defd4cdda0d11a35cf
Gabrielle <gabrielle.odhiambo@student.moringaschool.com>
2023-12-18 08:33:01 +0300

    customer class methods

--- Customer.py
+++ Customer.py
@@ -1,7 +1,11 @@
 class Customer:
+    customers= []
+
+
     def __init__(self, given_name, last_name):
         self.given_name= given_name
         self.last_name = last_name
+        Customer.customers.append(self)
     def given_name(self):
         return self.given_name
     def last_name(self):
@@ -9,7 +13,17 @@ class Customer:
     def full_name(self):
         return (f'{self.given_name}') +  (f'{self.last_name}')
     def all_names(self):
-        return self.Customer  
+        return self.Customer 
+    
+    @classmethod
+    def all(cls):
+        return cls.customers
+    def num_reviews(self):
+        return
+    def find_by_name(cls, name):
+        return
+    def find_all_by_given_name(cls, name):
+        return
 
 customer_one = Customer('George', 'Washington')
 print(customer_one.full_name())
